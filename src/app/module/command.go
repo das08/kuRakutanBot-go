371d1bf20f76f4fd182a21ee760dd116d3099d71
das08 <41512077+das08@users.noreply.github.com>
2022-03-29 11:53:07 +0900

    fix: refactored

--- src/app/module/command.go
+++ src/app/module/command.go
@@ -21,6 +21,9 @@ var Commands = [...]Command{
 	{Keyword: "楽単おみくじ", SendFunction: rakutanCmd},
 	{Keyword: "鬼単", SendFunction: onitanCmd},
 	{Keyword: "鬼単おみくじ", SendFunction: onitanCmd},
+	{Keyword: "お問い合わせ", SendFunction: inquiryCmd},
+	{Keyword: "問い合わせ", SendFunction: inquiryCmd},
+	{Keyword: "京大楽単bot", SendFunction: infoCmd},
 }
 
 func IsCommand(messageText string) (bool, func(env *Environments, lb *LINEBot)) {
@@ -37,17 +40,28 @@ func IsCommand(messageText string) (bool, func(env *Environments, lb *LINEBot)) 
 }
 
 func helpCmd(_ *Environments, lb *LINEBot) {
-	help := LoadHelp()
-	helpJson, _ := help.Marshal()
-	flexMessages := CreateFlexMessage(helpJson, "ヘルプ")
+	flexMessages := loadFlexMessages("./assets/richmenu/help.json", "ヘルプ")
 	lb.SendFlexMessage(flexMessages)
 }
 
 func judgeDetailCmd(_ *Environments, lb *LINEBot) {
-	judgeDetail := LoadJudgeDetail()
-	judgeDetailJson, _ := judgeDetail.Marshal()
-	flexMessages := CreateFlexMessage(judgeDetailJson, "らくたん判定の詳細")
+	flexMessages := loadFlexMessages("./assets/richmenu/judge_detail.json", "らくたん判定の詳細")
 	lb.SendFlexMessage(flexMessages)
+}
+
+func inquiryCmd(_ *Environments, lb *LINEBot) {
+	flexMessages := loadFlexMessages("./assets/richmenu/inquiry.json", "お問い合わせ")
+	lb.SendFlexMessage(flexMessages)
+}
+
+func infoCmd(_ *Environments, lb *LINEBot) {
+	flexMessages := loadFlexMessages("./assets/richmenu/info.json", "京大楽単bot")
+	lb.SendFlexMessage(flexMessages)
+}
+
+func loadFlexMessages(filename string, altText string) []FlexMessage {
+	json := LoadJSON(filename)
+	return CreateFlexMessage(json, altText)
 }
 
 func rakutanCmd(env *Environments, lb *LINEBot) {

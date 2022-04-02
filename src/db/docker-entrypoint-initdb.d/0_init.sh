632fe88a959967b05b589716bcfddac00e97ded2
das08 <41512077+das08@users.noreply.github.com>
2022-04-02 20:39:43 +0900

    add: init data for mongodb

---
+++ src/db/docker-entrypoint-initdb.d/0_init.sh
@@ -0,0 +1,1 @@
+mongoimport --authenticationDatabase admin --username rakutanbot --password QoS64f4wXSeoj67Or2u5 --db rakutanDB --collection rakutan2021 --drop --file /docker-entrypoint-initdb.d/rakutan2021.json --jsonArray

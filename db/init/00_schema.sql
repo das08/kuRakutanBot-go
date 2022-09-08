4c9c5575b35ba603c742445631177ce5002cf153
das08 <41512077+das08@users.noreply.github.com>
2022-09-08 18:43:33 +0900

    add schema for postgres

---
+++ db/init/00_schema.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE rakutan;
+
+\c rakutan
+
+CREATE TYPE user_action AS ENUM ('search', 'rakutan', 'onitan', 'set_fav', 'unset_fav', 'get_fav', 'info', 'help');
+
+CREATE TABLE users (
+    uid VARCHAR(64) NOT NULL,
+    is_verified BOOLEAN NOT NULL,
+    registered_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    verified_at TIMESTAMP WITH TIME ZONE,
+    PRIMARY KEY (uid)
+);
+
+CREATE TABLE user_logs (
+    uid VARCHAR(64) NOT NULL,
+    action user_action NOT NULL,
+    timestamp TIMESTAMP WITH TIME ZONE NOT NULL,
+    PRIMARY KEY (uid, timestamp)
+);
+
+CREATE TABLE favorites (
+    uid VARCHAR(64) NOT NULL,
+    id INTEGER NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (uid, id)
+);
+
+CREATE TABLE rakutan2021 (
+    id INTEGER NOT NULL,
+    faculty_name VARCHAR(64) NOT NULL,
+    lecture_name VARCHAR(64) NOT NULL,
+    register SMALLINT[],
+    passed SMALLINT[],
+    PRIMARY KEY (id)
+);
---
+++ db/init/10_index.sql
@@ -0,0 +1,1 @@
+CREATE INDEX idx_rakutan2021_lecture_name ON rakutan2021 (lecture_name);
